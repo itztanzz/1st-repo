1d523b74520465f92e6cf8771cef45a3a8d41e32
itztanzz <ttg.13032007@gmail.com>
2026-09-17 12:55:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char name[20];
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Hello %s!\n", name);
+    printf("Welcome to robotics and automation!\n");
+
+    return 0;
+}
